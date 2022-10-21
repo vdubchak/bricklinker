686fffd0be7bb73c8135ce7a2ba11c2860a52022
viacheslavdubchak <vdubchak@gmail.com>
2022-10-21 13:36:46 +0300

    Forced java server port to prevent heroku changing it and adding possibility for heroku provided port be binded to javaline

---
+++ src/main/java/com/vdubchak/telegrambricklinkbot/configuration/ServerCustomizer.java
@@ -0,0 +1,15 @@
+package com.vdubchak.telegrambricklinkbot.configuration;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServerCustomizer
+        implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        factory.setPort(8086);
+    }
+}
