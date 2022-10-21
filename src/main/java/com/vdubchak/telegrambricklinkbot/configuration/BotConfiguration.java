0e1cb44cee969b4d495ea3816f4e18765e516b0f
viacheslavdubchak <vdubchak@gmail.com>
2022-10-21 10:50:30 +0300

    Added okHttp client;

--- src/main/java/com/vdubchak/telegrambricklinkbot/configuration/BotConfiguration.java
+++ src/main/java/com/vdubchak/telegrambricklinkbot/configuration/BotConfiguration.java
@@ -3,8 +3,11 @@ package com.vdubchak.telegrambricklinkbot.configuration;
 import com.github.kshashov.telegram.config.TelegramBotGlobalProperties;
 import com.github.kshashov.telegram.config.TelegramBotGlobalPropertiesConfiguration;
 import com.pengrad.telegrambot.request.SetWebhook;
+import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class BotConfiguration implements TelegramBotGlobalPropertiesConfiguration {
@@ -18,8 +21,15 @@ public class BotConfiguration implements TelegramBotGlobalPropertiesConfiguratio
     private int port;
 
     @Override
-    public void configure(TelegramBotGlobalProperties.Builder builder) {
-        builder.setWebserverPort(port)
-                .configureBot(token, botBuilder -> botBuilder.useWebhook(new SetWebhook().url(url)));
+    public void configure(TelegramBotGlobalProperties.Builder telegramBotBuilder) {
+
+        OkHttpClient okHttp = new OkHttpClient.Builder()
+                .connectTimeout(12, TimeUnit.SECONDS)
+                .build();
+        telegramBotBuilder.setWebserverPort(port)
+                .configureBot(token, botBuilder -> {
+                    botBuilder.configure(builder -> builder.okHttpClient(okHttp));
+                    botBuilder.useWebhook(new SetWebhook().url(url));
+                });
     }
 }
