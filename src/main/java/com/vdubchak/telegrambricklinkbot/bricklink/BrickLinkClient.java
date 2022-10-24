804e9b859e8adc8688b35095517105e5a90dc6be
viacheslavdubchak <vdubchak@gmail.com>
2022-10-24 14:44:26 +0300

    Added retry attempts for faulty requests to BL

--- src/main/java/com/vdubchak/telegrambricklinkbot/bricklink/BrickLinkClient.java
+++ src/main/java/com/vdubchak/telegrambricklinkbot/bricklink/BrickLinkClient.java
@@ -1,11 +1,15 @@
 package com.vdubchak.telegrambricklinkbot.bricklink;
 
+import com.vdubchak.telegrambricklinkbot.bricklink.entity.AbstractBricklinkEntity;
 import com.vdubchak.telegrambricklinkbot.bricklink.entity.BricklinkInfoEntity;
 import com.vdubchak.telegrambricklinkbot.bricklink.entity.BricklinkPriceEntity;
+import lombok.SneakyThrows;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.security.auth.message.AuthException;
 import java.net.URI;
 
 @Service
@@ -17,6 +21,8 @@ public class BrickLinkClient {
         this.restTemplate = restTemplate;
     }
 
+    @SneakyThrows
+    @Retryable(value = {AuthException.class})
     public BricklinkPriceEntity getPrice(String set) {
 
         URI url = UriComponentsBuilder.fromHttpUrl("https://api.bricklink.com/api/store/v1/items/SET/" + set + "/price")
@@ -24,25 +30,39 @@ public class BrickLinkClient {
                 .queryParam("new_or_used", "N")
                 .build().toUri();
 
-        return restTemplate.getForEntity(url, BricklinkPriceEntity.class).getBody();
+        BricklinkPriceEntity result =  restTemplate.getForEntity(url, BricklinkPriceEntity.class).getBody();
+        checkResponse(result);
+        return result;
     }
 
-    public BricklinkPriceEntity getPrice(String set, String state) {
+    @SneakyThrows
+    @Retryable(value = {AuthException.class})
+    public BricklinkPriceEntity getPrice(String set, String state){
 
         URI url = UriComponentsBuilder.fromHttpUrl("https://api.bricklink.com/api/store/v1/items/SET/" + set + "/price")
                                       .queryParam("region", "eu")
                                       .queryParam("new_or_used", state)
                                       .build().toUri();
 
-        return restTemplate.getForEntity(url, BricklinkPriceEntity.class).getBody();
+        BricklinkPriceEntity result =  restTemplate.getForEntity(url, BricklinkPriceEntity.class).getBody();
+        checkResponse(result);
+        return result;
     }
 
+    @SneakyThrows
+    @Retryable(value = {AuthException.class})
     public BricklinkInfoEntity getInfo(String set) {
 
         URI url = UriComponentsBuilder.fromHttpUrl("https://api.bricklink.com/api/store/v1/items/SET/" + set)
                                       .build().toUri();
-
-        return restTemplate.getForEntity(url, BricklinkInfoEntity.class).getBody();
+        BricklinkInfoEntity result = restTemplate.getForEntity(url, BricklinkInfoEntity.class).getBody();
+        checkResponse(result);
+        return result;
     }
 
+    private void checkResponse(AbstractBricklinkEntity result) throws AuthException {
+        if(result == null || result.getMeta() == null || result.getMeta().getCode() != 200) {
+            throw new AuthException();
+        }
+    }
 }
